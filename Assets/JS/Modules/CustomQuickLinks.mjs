f81077cbb385f1893282ff48dc1993ea6423e3bd
Kian M <kianmahmoudi@outlook.com>
2019-06-05 10:17:17 -0700

    Update CustomQuickLinks.mjs

--- Assets/JS/Modules/CustomQuickLinks.mjs
+++ Assets/JS/Modules/CustomQuickLinks.mjs
@@ -5,4 +5,9 @@ export default {
 	rootSite: function() {
 		document.location = document.location.origin
 	},
+
+	// One time quick links
+	ot_2019_gds: function() {
+		document.location = document.location.origin + '/Special/School/GraphicDesignStudio2018-19/Portfolio.html'
+	}
 }
